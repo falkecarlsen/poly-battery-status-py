3cec98878cf933d010c47dd56eade5adb5c7d644
cogitantium <falke.cs@gmail.com>
2020-02-08 14:24:33 +0100

    [license] Fix license header in source

--- poly_bat.py
+++ poly_bat.py
@@ -1,5 +1,5 @@
 """
-<one line to give the program's name and a brief idea of what it does.>
+poly-battery-status-py: Generates a pretty status-bar string for multi-battery systems on Linux.
 Copyright (C) 2020  Falke Carlsen
 
 This program is free software: you can redistribute it and/or modify
